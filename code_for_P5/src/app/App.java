b4e7efa111254ed0fde8536c1222e716ad7dbff3
Anne Markussen <amarku17@student.aau.dk>
2019-11-15 13:29:21 +0100

    navn
    navn

--- code_for_P5/src/app/App.java
+++ code_for_P5/src/app/App.java
@@ -4,5 +4,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello Java");
         //Navn
+        // Mit navn er Anne :-)
     }
 }
