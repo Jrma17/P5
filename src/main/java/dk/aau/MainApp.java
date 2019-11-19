44ea0ecd7f96a0a16cdc9e15cdffbb86db77e3c6
Jrma17 <56030464+Jrma17@users.noreply.github.com>
2019-11-19 14:47:49 +0100

    Views til henvisning

--- src/main/java/dk/aau/MainApp.java
+++ src/main/java/dk/aau/MainApp.java
@@ -5,13 +5,14 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.BorderPane;
+
+
 import javafx.stage.Stage;
 
 public class MainApp extends Application {
 
     private Stage primaryStage;
-    private BorderPane mainView;
+    private AnchorPane mainView;
 
     @Override
     public void start(Stage primaryStage) {
@@ -20,7 +21,8 @@ public class MainApp extends Application {
 
         initRootLayout();
 
-        showPersonOverview();
+
+        
     }
     
     /**
@@ -30,8 +32,8 @@ public class MainApp extends Application {
         try {
             // Load root layout from fxml file.
             FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(MainApp.class.getResource("/view/MainView.fxml"));
-            mainView = (BorderPane) loader.load();
+            loader.setLocation(MainApp.class.getResource("/MainView.fxml"));
+            mainView = (AnchorPane) loader.load();
             
             // Show the scene containing the root layout.
             Scene scene = new Scene(mainView);
@@ -45,20 +47,21 @@ public class MainApp extends Application {
     /**
      * Shows the person overview inside the root layout.
      */
-    public void showPersonOverview() {
-        try {
-            // Load person overview.
-            FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(MainApp.class.getResource("/view/ReferralButton.fxml"));
-            AnchorPane ReferralButton = (AnchorPane) loader.load();
+    // public void showReferralButton() {
+    //      try {
+    //          // Load person overview.
+    //          FXMLLoader loader = new FXMLLoader();
+    //          loader.setLocation(MainApp.class.getResource("/view/ReferralButton.fxml"));
+    //          AnchorPane ReferralButton = (AnchorPane) loader.load();
             
-            // Set person overview into the center of root layout.
-            mainView.setCenter(ReferralButton);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+    //          // Set person overview into the center of root layout.
+    //          mainView.setBottom(ReferralButton);
+    //      } catch (IOException e) {
+    //          e.printStackTrace();
+    //      }
+    //  }
     
+     
     /**
      * Returns the main stage.
      * @return
