9affcc609d10a1405d39a8cd769eab4fe2a9a0f2
Jrma17 <56030464+Jrma17@users.noreply.github.com>
2019-11-29 08:26:49 +0100

    Nyt

--- src/main/java/dk/aau/MainApp.java
+++ src/main/java/dk/aau/MainApp.java
@@ -2,8 +2,8 @@ package dk.aau;
 
 import java.io.IOException;
 
-import dk.aau.controller.MainCtrl;
 import dk.aau.controller.ReferralCtrl;
+import dk.aau.controller.ReferralHandler;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.collections.FXCollections;
@@ -11,15 +11,20 @@ import javafx.collections.ObservableList;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
-import dk.aau.controller.ReferralCtrl;
+
+import dk.aau.model.DatabaseManipulator;
 import dk.aau.model.PatientModel;
 import dk.aau.model.ReferralModel;
+import dk.aau.model.ReferralListModel;
 import dk.aau.model.VisitationModel;
 import javafx.stage.Stage;
 
 public class MainApp extends Application {
 
     private Stage primaryStage;
+    public static ReferralModel referralModel = new ReferralModel();
+    public static PatientModel patientModel = new PatientModel();
+    public static ReferralListModel referralListModel = new ReferralListModel();
     
     
      //Arrayliste med patientdata og henvisningsdata
@@ -37,8 +42,7 @@ public class MainApp extends Application {
          // patientData.add(new PatientModel("Werner", "Årsagvej 4", " 192835-1023"));
          // patientData.add(new PatientModel("Lydia", "Årsagvej 5", "101039-5302"));
         
-         referralData.add(new ReferralModel("12/1-2019", "12/1-2019", "Laegehus A","Hoste", 
-                  "Aarsag 1", "1234567", "ICPC-kode", "XML", "Afdeling 2"));
+         //referralData.add(new ReferralModel("12/1-2019", "12/1-2019", "Laegehus A","Hoste",  "Aarsag 1", "1234567", "ICPC-kode", "XML", "Afdeling 2"));
                   
      }
      
@@ -69,8 +73,13 @@ public class MainApp extends Application {
           
         public void showReferralListView() {
             try {
+               MainApp.referralModel =  ReferralHandler.readReferral();
+               MainApp.patientModel = ReferralHandler.readPatientData();
+               MainApp.referralListModel = ReferralHandler.readReferralList();
+               //MainApp.patientModel = PatientModel.readPatientData();
                 // Load Sort List View
                 FXMLLoader loader = new FXMLLoader();
+                
                 loader.setLocation(MainApp.class.getResource("/ReferralListView.fxml"));
                 AnchorPane ReferralListView = (AnchorPane) loader.load();
                 
@@ -81,9 +90,11 @@ public class MainApp extends Application {
                 // Show the scene containing the root layout.
                 Scene sceneReferralListView = new Scene(ReferralListView);
                 primaryStage.setScene(sceneReferralListView);
+                //ReferralCtrl controller = loader.getController();
                 primaryStage.show();
                
             } catch (IOException e) {
+                
                 e.printStackTrace();
             }
         }
@@ -99,6 +110,8 @@ public class MainApp extends Application {
     
         public static void main(String[] args) {
             launch(args);
+          
+        
         }
     
     }
--- src/main/java/dk/aau/controller/MainCtrl.java
+++ src/main/java/dk/aau/controller/MainCtrl.java
@@ -1,17 +1,9 @@
 package dk.aau.controller;
 
-import dk.aau.MainApp;
-import dk.aau.controller.*;
 import javafx.fxml.FXML;
 
 public class MainCtrl {
-    private ReferralListCtrl referralListCtrl;
-    @FXML public ReferralCtrl referralCtrl;
-    private MappingListCtrl mappingListCtrl;
-    private CatalogCtrl catalogCtrl;
-
     
-
-    
+   
         
 }
